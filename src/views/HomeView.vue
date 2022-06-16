a9ac432560f313cbd08b04bc98cd03d02355a9a4
shaunchenucsb <49256728+shaunchenucsb@users.noreply.github.com>
2022-06-16 01:55:18 -0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <i>Hello, I'm Shaun! I just finished my Bachelor's degree in Electrical Engineering at University of California, Santa Barbara. I plan to further pursue a Master's in Electrical and Computer Engineering with an Emphasis in Signal Processing.</i>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+// export default {
+//   name: 'HomeView',
+//   components: {
+//     HelloWorld
+//   }
+// }
+</script>
---
+++ src/views/MyResume.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="home">
+    <img src="@/assets/Shaun_Chen_Resume.jpg" alt="Resume" height="1080">
+    </div>
+</template>
