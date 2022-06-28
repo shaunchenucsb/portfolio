fe6c9cc45a446b8db68c5f6c13b0e9c873354fc9
shaunchenucsb <49256728+shaunchenucsb@users.noreply.github.com>
2022-06-28 03:31:41 -0700

    adding dist subtree

--- js/about.01a69d3b.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkportfolio"]=self["webpackChunkportfolio"]||[]).push([[443],{381:function(n,t,o){o.r(t),o.d(t,{default:function(){return l}});var r=o(252);const u={class:"about"},a=(0,r._)("h1",null,"This is an about page",-1),e=[a];function s(n,t){return(0,r.wg)(),(0,r.iD)("div",u,e)}var i=o(744);const c={},f=(0,i.Z)(c,[["render",s]]);var l=f}}]);
-//# sourceMappingURL=about.01a69d3b.js.map
